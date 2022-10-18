d6cc89356b967fd6ad74f2cb40c386aacfa3ac38
Aidan Cole <aidanjcole@gmail.com>
2022-10-18 18:58:42 -0400

    Introducing optional arguments

--- main.py
+++ main.py
@@ -3,10 +3,14 @@ import argparse
 parser = argparse.ArgumentParser()
 
 #strings can be bare when single or in single or double quotes when multiple words are needed.
-# paramaters for add_argument:
+# parameters for add_argument:
 # name, type, required.
 parser.add_argument('--name', type=str, required=True)
-
+# optional arguments are as simple as omitting required=True
+parser.add_argument('--age', type=int)
 args = parser.parse_args()
 
-print('Hello,', args.name)
+if args.age:
+    print(args.name, 'is', args.age, 'years old.')
+else:
+    print('Hello,', args.name)
