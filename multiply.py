68d52426828c5c4c8c4e45d603a70ae8cd9906f0
Aidan Cole <aidanjcole@gmail.com>
2022-10-18 18:54:43 -0400

    Added help strings to multiply.py

--- multiply.py
+++ multiply.py
@@ -4,8 +4,8 @@ import argparse
 parser = argparse.ArgumentParser()
 # x and y are positional arguments
 # positional arguments can't be referenced with the '--' prefix
-parser.add_argument('x', type=int)
-parser.add_argument('y', type=int)
+parser.add_argument('x', type=int, help='The first value to multiply')
+parser.add_argument('y', type=int, help='The second value to multiply')
 args = parser.parse_args()
 
 product = args.x * args.y
