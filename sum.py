8c99608ae39d7ed4ee927706c6e1677c39248305
Aidan Cole <aidanjcole@gmail.com>
2022-10-19 19:43:24 -0400

    Added flag alias example in sum.py

--- sum.py
+++ sum.py
@@ -3,7 +3,7 @@ import argparse
 parser = argparse.ArgumentParser()
 # multiple arguments can be passed with nargs set to a value, must be exact
 # an arbitrary number of 1+ arguments can be given with the string '+'
-parser.add_argument('--values', type=int, nargs='+')
+parser.add_argument('--values', '-v', type=int, nargs='+')
 args = parser.parse_args()
 
 sum= sum(args.values)
