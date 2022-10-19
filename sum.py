d33acbd47b7da99c0005e6044fdf687d761fb681
Aidan Cole <aidanjcole@gmail.com>
2022-10-19 19:12:25 -0400

    Converted multiple arguments from set number to variable

--- sum.py
+++ sum.py
@@ -1,7 +1,9 @@
 import argparse
 
 parser = argparse.ArgumentParser()
-parser.add_argument('--values', type=int, nargs=3)
+# multiple arguments can be passed with nargs set to a value, must be exact
+# an arbitrary number of 1+ arguments can be given with the string '+'
+parser.add_argument('--values', type=int, nargs='+')
 args = parser.parse_args()
 
 sum= sum(args.values)
